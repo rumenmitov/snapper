7d5da337dcd2eb3d9a20192b96af79c31aa177ab
Rumen <rumenmitov@protonmail.com>
2025-06-12 15:02:54 +0200

    feat: pretty printing test output

--- src/tests/snappertests.cc
+++ src/tests/snappertests.cc
@@ -3,36 +3,49 @@
 #include "snapper.h"
 
 /* INFO __PRETTY_FUNCTION__ is only available on gcc and clang. */
-#ifndef __PRETTY_FUNCTION__
-#define __PRETTY_FUNCTION__ "Test"
-#endif // __PRETTY_FUNCTION__
+// #ifndef __PRETTY_FUNCTION__
+// #define __PRETTY_FUNCTION__ "Test"
+// #endif // __PRETTY_FUNCTION__
 
 /* Colors */
 #define RED "\033[31m"
 #define GREEN "\033[32m"
 #define RESET "\033[0m"
 
-/* Assert */
-#define ASSERT(condition)                                                      \
-  {                                                                            \
-    if (condition) {                                                           \
-      Genode::log(GREEN __PRETTY_FUNCTION__ " passed!" RESET);                 \
-    } else {                                                                   \
-      Genode::log(RED __PRETTY_FUNCTION__ " failed!" RESET);                   \
-    }                                                                          \
+/* Test Stats */
+static unsigned total_tests = 0;
+static unsigned successful_tests = 0;
+
+/* Run Test */
+void test(const char *name, bool condition) {
+  total_tests++;
+  
+  if (condition) {
+    successful_tests++;
+    Genode::log(GREEN, name, " passed!" RESET);
+  } else {
+    Genode::log(RED, name, " failed!" RESET);
   }
+}
+
+/* Summarize Test Results */
+void summary(void) 
+{
+  Genode::log("\n", successful_tests, "/", total_tests, " tests passed.");
+}
 
 using namespace SnapperNS;
 
-void test_initialization(void) 
-{
+void test_initialization(void) {
   snapper = Snapper::new_snapper();
-  ASSERT(snapper);
+  test(__PRETTY_FUNCTION__, snapper);
 }
 
-void Component::construct(Genode::Env &) 
-{
-  Genode::log("-*- SNAPPER TEST SUITE -*-");
+void Component::construct(Genode::Env &) {
+  Genode::log("-*- SNAPPER TEST SUITE -*-\n");
+
   test_initialization();
-  Genode::log("-*- SNAPPER TESTS DONE -*-");
+
+  summary();
+  Genode::log("\n-*- SNAPPER TESTS DONE -*-");
 }
